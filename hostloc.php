e1ddb7c21a740a03033a784903cb8e6af4d226d8
Cyril Wong <kli@live.com>
2018-09-22 18:41:23 +0000

    Update hostloc.php

--- hostloc.php
+++ hostloc.php
@@ -1,46 +1,66 @@
-<?php
-$useList = array(
-                                '西海岸的故事'=>'Li1314..',
-                                '家有自己自温暖'=>'Li1314..',
-                                '账号3'=>'密码3'
-                                );
 
-foreach($useList as $k=>$v){
-        locSign($k,$v);
+
+<?php
+
+$username = "我是大傻瓜";  //LOC账户
+$password = "qwertyuio";//LOC密码
+
+
+//*******************以下不用修改*******************
+
+set_time_limit(600);
+
+$suburl = "https://www.hostloc.com/member.php?mod=logging&action=login";
+$loginInfo = array(
+        "username" => $username,
+        "password" => $password,
+        "fastloginfield" => "username",
+        "quickforward" => "yes",
+        "handlekey" => "ls",
+        "loginsubmit" => true
+);
+
+
+$login = postData($suburl,$loginInfo);
+
+$spaceUrl = "https://www.hostloc.com/space-uid-*.html";
+
+for($i=20940;$i<20952;$i++)
+{
+        getData(str_replace("*",$i,$spaceUrl));
 }
-function locSign($use,$pwd){
-        $html = http_post('https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1','fastloginfield=username&username='.urlencode($use).'&cookietime=2592000&password='.urlencode($pwd).'&quickforward=yes&handlekey=ls');
-        if(!strrpos($html,'window.location.href')){echo $use.' login error<br>';return false;}
-        $cookie = getCookie($html,'Set-Cookie: ',';');
-        for($i=26200; $i<=26210; $i++){
-                $html = http_get("https://www.hostloc.com/space-uid-{$i}.html",$cookie);
-        }
-        echo $use.' Sign ok!!!<br>';
+
+function postData($url, $post_data)
+{
+        $ch = curl_init ();
+        curl_setopt($ch , CURLOPT_POST , 1);
+        curl_setopt($ch , CURLOPT_HEADER , 0);
+        curl_setopt($ch , CURLOPT_URL , $url);
+        curl_setopt($ch , CURLOPT_COOKIEJAR , 'hostloc.cookie');
+        curl_setopt($ch , CURLOPT_POSTFIELDS , $post_data);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,600);
+        curl_setopt($ch, CURLOPT_REFERER, 'https://www.hostloc.com/'); 
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
 }
-function http_get($url,$cookie) {
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_HTTPHEADER,array('User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0','Cookie: '.$cookie));
-        curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-        $src = curl_exec($curl);
-        curl_close($curl);
-        return $src;
+
+
+function getData($url)
+{
+        $ch = curl_init ();
+        curl_setopt($ch, CURLOPT_HEADER , 0);
+        curl_setopt($ch, CURLOPT_URL , $url);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'hostloc.cookie');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,600);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
 }
-function http_post($url,$body){
-        $ch = curl_init();  
-        curl_setopt($ch, CURLOPT_URL,$url);  
-        curl_setopt($ch, CURLOPT_HEADER, TRUE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-        $result = curl_exec($ch);  
-        curl_close($ch);  
-        return $result;    
-}
-function getCookie($str,$leftStr,$rightStr){
-        if(strrpos($str,$leftStr) == false || strrpos($str,$rightStr) == false){return '';}
-        $arr1 = explode($leftStr,$str);$i = 0;$cookie = '';
-        foreach($arr1 as $k=>$v){$i++;if($i > 1){$arr2 = explode($rightStr,$v);$cookie .= $arr2[0].'; ';}}
-        return $cookie;
-}
+
